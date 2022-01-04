eb17334719ffc99f953889766824005c9891e64f
Anny Cárdenas <ajcardenasb1@gmail.com>
2022-01-04 11:32:33 +0100

    Create R_introduction.R

---
+++ R_introduction.R
@@ -0,0 +1,14 @@
+#1. set your working directory using the "setwd" function
+
+#2. load required libraries using the "library" function
+
+#3. import and inspect data using the 
+
+
+#4. filter dataframe to keep only individuals who have banana as their favorite fruit
+#5. filter dataframe to keep only individuals older than 50
+#6. calculate the average age of per favorite holiday
+#7. calculate the total number of pets per fruit
+#8. create a new column with the structure "ID - fruit - holiday"
+#9. replace mango by blueberry
+#10. create a new column with young/old (young= < 50)
